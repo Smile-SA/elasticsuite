35af793229196a053a02352cb72c85d867839c48
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-03-07 09:04:02 +0100

    Spellchecker specification and implementation

---
+++ src/Api/Search/Spellchecker/RequestInterface.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_ElasticSuiteCore
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticSuiteCore\Api\Search\Spellchecker;
+
+/**
+ * Spellchecking request interface.
+ *
+ * @category Smile
+ * @package  Smile_ElasticSuiteCore
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+interface RequestInterface
+{
+    /**
+     * Spellcheck request index name.
+     *
+     * @return string
+     */
+    public function getIndex();
+
+    /**
+     * Spellcheck request document type.
+     *
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * Spellcheck fulltext query.
+     *
+     * @return string
+     */
+    public function getQueryText();
+
+    /**
+     * Spellcheck cutoff frequency (used to detect stopwords).
+     *
+     * @return float
+     */
+    public function getCutoffFrequency();
+}
---
+++ src/Api/Search/SpellcheckerInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_ElasticSuiteCore
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticSuiteCore\Api\Search;
+
+use Smile\ElasticSuiteCore\Api\Search\Spellchecker\RequestInterface;
+
+/**
+ * Spellchecker component interface.
+ *
+ * @category Smile
+ * @package  Smile_ElasticSuiteCore
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+interface SpellcheckerInterface
+{
+    const SPELLING_TYPE_EXACT          = 1;
+    const SPELLING_TYPE_MOST_EXACT     = 2;
+    const SPELLING_TYPE_MOST_FUZZY     = 3;
+    const SPELLING_TYPE_FUZZY          = 4;
+    const SPELLING_TYPE_PURE_STOPWORDS = 5;
+
+    /**
+     * Returns the type of spelling of a fultext query :
+     *
+     * - SPELLING_TYPE_EXACT          : All terms of the text query exist and are exactly spelled.
+     * - SPELLING_TYPE_MOST_EXACT     : All of the text query terms exist. Some are types using an analyzed form.
+     * - SPELLING_TYPE_MOST_FUZZY     : At least one term of the text query was exists in the index.
+     * - SPELLING_TYPE_FUZZY          : All terms of the text query was mispelled.
+     * - SPELLING_TYPE_PURE_STOPWORDS : All terms of the text query exist and are stopwords (a, the, ...).
+     *
+     * @param RequestInterface $request Spellchecking query.
+     *
+     * @return integer
+     */
+    public function getSpellingType(RequestInterface $request);
+}
