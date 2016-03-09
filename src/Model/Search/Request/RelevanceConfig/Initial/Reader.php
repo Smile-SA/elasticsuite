890f9a2c9a138b79189ae25af66f94aefd55b05a
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-03-09 11:50:47 +0100

    Simply di.xml

--- src/Model/Search/Request/RelevanceConfig/Initial/Reader.php
+++ src/Model/Search/Request/RelevanceConfig/Initial/Reader.php
@@ -12,7 +12,9 @@
  */
 namespace Smile\ElasticSuiteCore\Model\Search\Request\RelevanceConfig\Initial;
 
-use Smile\ElasticSuiteCore\Model\Search\Request\RelevanceConfig\Initial\SchemaLocator;
+use Magento\Framework\Config\FileResolverInterface;
+use Magento\Framework\Config\DomFactory;
+use Magento\Framework\Config\ConverterInterface;
 
 /**
  * Relevance configuration initial reader
@@ -29,17 +31,17 @@ class Reader extends \Magento\Framework\App\Config\Initial\Reader
     const FILE_NAME = 'elasticsuite_relevance.xml';
 
     /**
-     * @param \Magento\Framework\Config\FileResolverInterface $fileResolver  The file resolver
-     * @param \Magento\Framework\Config\ConverterInterface    $converter     Converter
-     * @param SchemaLocator                                   $schemaLocator The schema locator
-     * @param \Magento\Framework\Config\DomFactory            $domFactory    The DOM factory
-     * @param string                                          $fileName      The file name
+     * @param FileResolverInterface $fileResolver  The file resolver
+     * @param ConverterInterface    $converter     Converter
+     * @param SchemaLocator         $schemaLocator The schema locator
+     * @param DomFactory            $domFactory    The DOM factory
+     * @param string                $fileName      The file name
      */
     public function __construct(
-        \Magento\Framework\Config\FileResolverInterface $fileResolver,
-        \Magento\Framework\Config\ConverterInterface $converter,
+        FileResolverInterface $fileResolver,
+        ConverterInterface $converter,
         SchemaLocator $schemaLocator,
-        \Magento\Framework\Config\DomFactory $domFactory,
+        DomFactory $domFactory,
         $fileName = self::FILE_NAME
     ) {
         parent::__construct($fileResolver, $converter, $schemaLocator, $domFactory, $fileName);
