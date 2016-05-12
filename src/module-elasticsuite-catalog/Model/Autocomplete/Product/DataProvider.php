63d0d676979929310f4620875427419e91c8b306
Romain Ruaud <romain.ruaud@smile.fr>
2016-05-12 16:17:03 +0200

    Dismantle previous autocomplete module and dispatch it between Core and Catalog modules.

---
+++ src/module-elasticsuite-catalog/Model/Autocomplete/Product/DataProvider.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_ElasticSuiteCatalog
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticSuiteCatalog\Model\Autocomplete\Product;
+
+use Magento\Search\Model\Autocomplete\DataProviderInterface;
+use Magento\Search\Model\Autocomplete\ItemFactory;
+use Magento\Search\Model\QueryFactory;
+use Smile\ElasticSuiteCore\Helper\Autocomplete as ConfigurationHelper;
+use Smile\ElasticSuiteCatalog\Model\ResourceModel\Product\Fulltext\CollectionFactory as ProductCollectionFactory;
+use Smile\ElasticSuiteCore\Model\Autocomplete\Terms\DataProvider as TermDataProvider;
+
+/**
+ * Catalog product autocomplete data provider.
+ *
+ * @category Smile
+ * @package  Smile_ElasticSuiteCatalog
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class DataProvider implements DataProviderInterface
+{
+    /**
+     * Autocomplete type
+     */
+    const AUTOCOMPLETE_TYPE = "product";
+
+    /**
+     * Autocomplete result item factory
+     *
+     * @var ItemFactory
+     */
+    protected $itemFactory;
+
+    /**
+     * Query factory
+     *
+     * @var QueryFactory
+     */
+    protected $queryFactory;
+
+    /**
+     * @var TermDataProvider
+     */
+    protected $termDataProvider;
+
+    /**
+     * @var ProductCollectionFactory
+     */
+    protected $productCollectionFactory;
+
+    /**
+     * @var \Magento\Catalog\Helper\Product
+     */
+    protected $imageHelper;
+
+    /**
+     * @var ConfigurationHelper
+     */
+    protected $configurationHelper;
+
+    /**
+     * @var string Autocomplete result type
+     */
+    private $type;
+
+    /**
+     * Constructor.
+     *
+     * @param ItemFactory                     $itemFactory              Suggest item factory.
+     * @param QueryFactory                    $queryFactory             Search query factory.
+     * @param TermDataProvider                $termDataProvider         Search terms suggester.
+     * @param ProductCollectionFactory        $productCollectionFactory Product collection factory.
+     * @param \Magento\Catalog\Helper\Product $productHelper            Catalog Image helper.
+     * @param ConfigurationHelper             $configurationHelper      Autocomplete configuration helper.
+     * @param string                          $type                     Autocomplete provider type.
+     */
+    public function __construct(
+        ItemFactory $itemFactory,
+        QueryFactory $queryFactory,
+        TermDataProvider $termDataProvider,
+        ProductCollectionFactory $productCollectionFactory,
+        \Magento\Catalog\Helper\Product $productHelper,
+        ConfigurationHelper $configurationHelper,
+        $type = self::AUTOCOMPLETE_TYPE
+    ) {
+        $this->itemFactory              = $itemFactory;
+        $this->queryFactory             = $queryFactory;
+        $this->termDataProvider         = $termDataProvider;
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->imageHelper              = $productHelper;
+        $this->configurationHelper      = $configurationHelper;
+        $this->type                     = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getItems()
+    {
+        $result = [];
+        $productCollection = $this->getProductCollection();
+        if ($productCollection) {
+            foreach ($productCollection as $product) {
+                $result[] = $this->itemFactory->create(
+                    [
+                        'title'       => $product->getName(),
+                        'image'       => $this->imageHelper->getSmallImageUrl($product),
+                        'url'         => $product->getProductUrl(),
+                        'price'       => $product->getFinalPrice(),
+                        'final_price' => $product->getPrice(), // The getPrice method returns always 0.
+                        'type'        => $this->getType(),
+                    ]
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * List of search terms suggested by the search terms data daprovider.
+     *
+     * @return array
+     */
+    private function getSuggestedTerms()
+    {
+        $terms = array_map(
+            function (\Magento\Search\Model\Autocomplete\Item $termItem) {
+                return $termItem->getTitle();
+            },
+            $this->termDataProvider->getItems()
+        );
+
+        return $terms;
+    }
+
+    /**
+     * Suggested products collection.
+     * Returns null if no suggested search terms.
+     *
+     * @return \Smile\ElasticSuiteCatalog\Model\ResourceModel\Product\Fulltext\Collection|null
+     */
+    private function getProductCollection()
+    {
+        $productCollection = null;
+        $suggestedTerms = $this->getSuggestedTerms();
+        $terms          = [$this->queryFactory->get()->getQueryText()];
+
+        if (!empty($suggestedTerms)) {
+            $terms = array_merge($terms, $suggestedTerms);
+        }
+
+        $productCollection = $this->productCollectionFactory->create();
+        $productCollection->addSearchFilter($terms);
+        $productCollection->setPageSize($this->getResultsPageSize());
+        $productCollection
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('small_image')
+            ->addPriceData();
+
+        return $productCollection;
+    }
+
+    /**
+     * Retrieve number of products to display in autocomplete results
+     *
+     * @return int
+     */
+    private function getResultsPageSize()
+    {
+        return $this->configurationHelper->getMaxSize($this->getType());
+    }
+}
---
+++ src/module-elasticsuite-core/Model/Autocomplete/Terms/DataProvider.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_ElasticSuiteCore
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticSuiteCore\Model\Autocomplete\Terms;
+
+use Magento\Search\Model\ResourceModel\Query\Collection;
+use Magento\Search\Model\QueryFactory;
+use Magento\Search\Model\Autocomplete\DataProviderInterface;
+use Magento\Search\Model\Autocomplete\ItemFactory;
+use Smile\ElasticSuiteCore\Helper\Autocomplete as ConfigurationHelper;
+
+/**
+ * Popular search terms data provider.
+ *
+ * @category Smile
+ * @package  Smile_ElasticSuiteCore
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class DataProvider implements DataProviderInterface
+{
+    /**
+     * Autocomplete type
+     */
+    const AUTOCOMPLETE_TYPE = "term";
+
+    /**
+     * Query factory
+     *
+     * @var QueryFactory
+     */
+    protected $queryFactory;
+
+    /**
+     * Autocomplete result item factory
+     *
+     * @var ItemFactory
+     */
+    protected $itemFactory;
+
+    /**
+     * @var ConfigurationHelper
+     */
+    protected $configurationHelper;
+
+    /**
+     *
+     * @var \Magento\Search\Model\Autocomplete\Item[]
+     */
+    private $items;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * Constructor.
+     *
+     * @param QueryFactory        $queryFactory        Search query text factory.
+     * @param ItemFactory         $itemFactory         Suggest terms item facory.
+     * @param ConfigurationHelper $configurationHelper Autocomplete configuration helper.
+     * @param string              $type                Autocomplete items type.
+     */
+    public function __construct(
+        QueryFactory $queryFactory,
+        ItemFactory $itemFactory,
+        ConfigurationHelper $configurationHelper,
+        $type = self::AUTOCOMPLETE_TYPE
+    ) {
+        $this->queryFactory        = $queryFactory;
+        $this->itemFactory         = $itemFactory;
+        $this->configurationHelper = $configurationHelper;
+        $this->type                = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItems()
+    {
+        if ($this->items === null) {
+            $collection = $this->getSuggestCollection();
+            $this->items = [];
+            foreach ($collection as $item) {
+                $resultItem = $this->itemFactory->create([
+                    'title' => $item->getQueryText(),
+                    'num_results' => $item->getNumResults(),
+                    'type'        => $this->getType(),
+                ]);
+                $this->items[] = $resultItem;
+            }
+        }
+
+        return $this->items;
+    }
+
+    /**
+     * Retrieve suggest collection for query
+     *
+     * @return Collection
+     */
+    private function getSuggestCollection()
+    {
+        $queryCollection = $this->queryFactory->get()->getSuggestCollection();
+        $queryCollection->addFieldToFilter('is_spellchecked', 'false')->setPageSize($this->getResultsPageSize());
+
+        return $queryCollection;
+    }
+
+    /**
+     * Retrieve number of products to display in autocomplete results
+     *
+     * @return int
+     */
+    private function getResultsPageSize()
+    {
+        return $this->configurationHelper->getMaxSize($this->getType());
+    }
+}
