efe0c5fb16ca2d6dc843640fec66326b0194e629
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-05-13 06:03:13 +0200

    Better code structure for product autocomplete.

---
+++ src/module-elasticsuite-catalog/Model/Autocomplete/Product/ItemFactory.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_ElasticSuiteCatalog
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticSuiteCatalog\Model\Autocomplete\Product;
+
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Catalog\Helper\Product as ProductHelper;
+
+/**
+ * Create an autocomplete item from a product.
+ *
+ * @category Smile
+ * @package  Smile_ElasticSuiteCatalog
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class ItemFactory extends \Magento\Search\Model\Autocomplete\ItemFactory
+{
+    /**
+     * @var ProductHelper
+     */
+    private $productHelper;
+
+    /**
+     * Constructor.
+     *
+     * @param ObjectManagerInterface $objectManager Object manager used to instantiate new item.
+     * @param ProductHelper          $productHelper Catalog product helper.
+     */
+    public function __construct(ObjectManagerInterface $objectManager, ProductHelper $productHelper)
+    {
+        parent::__construct($objectManager);
+        $this->productHelper = $productHelper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function create(array $data)
+    {
+        $data = $this->addProductData($data);
+        unset($data['product']);
+
+        return parent::create($data);
+    }
+
+    /**
+     * Load product data and append them to the original data.
+     *
+     * @param array $data Autocomplete item data.
+     *
+     * @return array
+     */
+    private function addProductData($data)
+    {
+        $product = $data['product'];
+
+        $productData = [
+            'title'       => $product->getName(),
+            'image'       => $this->productHelper->getSmallImageUrl($product),
+            'url'         => $product->getProductUrl(),
+            'price'       => $product->getFinalPrice(),
+            'final_price' => $product->getPrice(),
+        ];
+
+        $data = array_merge($data, $productData);
+
+        return $data;
+    }
+}
