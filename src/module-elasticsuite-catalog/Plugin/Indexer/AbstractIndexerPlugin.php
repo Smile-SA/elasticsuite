ffdf1dca7a4ec4684c757025c1af7662e8a18b5c
Romain Ruaud <romain.ruaud@smile.fr>
2016-06-02 17:44:50 +0200

    Ensure parent products are reindexed when reindexing a child one.

--- src/module-elasticsuite-catalog/Plugin/Indexer/AbstractIndexerPlugin.php
+++ src/module-elasticsuite-catalog/Plugin/Indexer/AbstractIndexerPlugin.php
@@ -12,6 +12,8 @@
  */
 namespace Smile\ElasticSuiteCatalog\Plugin\Indexer;
 
+use Magento\Framework\App\ResourceConnection;
+
 /**
  * Generic indexer plugin, handling fulltext index process
  *
@@ -30,10 +32,12 @@ class AbstractIndexerPlugin
      * ReindexProductsAfterSave constructor.
      *
      * @param \Magento\Framework\Indexer\IndexerRegistry $indexerRegistry The indexer registry
+     * @param ResourceConnection                         $resource        The Resource connection
      */
-    public function __construct(\Magento\Framework\Indexer\IndexerRegistry $indexerRegistry)
+    public function __construct(\Magento\Framework\Indexer\IndexerRegistry $indexerRegistry, ResourceConnection $resource)
     {
         $this->indexerRegistry = $indexerRegistry;
+        $this->resource        = $resource;
     }
 
     /**
@@ -45,13 +49,34 @@ class AbstractIndexerPlugin
     {
         $fullTextIndexer = $this->indexerRegistry->get(\Magento\CatalogSearch\Model\Indexer\Fulltext::INDEXER_ID);
 
+        if (!is_array($ids)) {
+            $ids = [$ids];
+        }
+
+        $parentIds  = $this->getRelationsByChild($ids);
+        $processIds = $parentIds ? array_merge($parentIds, $ids) : $ids;
+
         if (!$fullTextIndexer->isScheduled()) {
-            if (is_array($ids) && (!empty($ids))) {
-                $fullTextIndexer->reindexList($ids);
-            }
-            if (!is_array($ids) && is_numeric($ids)) {
-                $fullTextIndexer->reindexRow($ids);
+            if (!empty($processIds)) {
+                $fullTextIndexer->reindexList($processIds);
             }
         }
+    }
+
+    /**
+     * Retrieve products relations by childrens
+     *
+     * @param array $childrenIds The product ids being reindexed
+     *
+     * @return array
+     */
+    private function getRelationsByChild($childrenIds)
+    {
+        $connection = $this->resource->getConnection();
+        $select = $connection->select()
+            ->from($this->resource->getTableName('catalog_product_relation'), 'parent_id')
+            ->where('child_id IN(?)', $childrenIds);
+
+        return $connection->fetchCol($select);
     }
 }
