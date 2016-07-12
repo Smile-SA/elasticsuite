b88a60cf5ed6c6bafebca470c53de86f6f0b48ac
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-07-12 06:47:25 +0200

    Fix #74 - Better admin for fuzziness config.

---
+++ src/module-elasticsuite-core/Model/Config/Source/FuzzinessValue.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCore
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticsuiteCore\Model\Config\Source;
+
+/**
+ * Fuzziness value config source model.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCore
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class FuzzinessValue implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function toOptionArray()
+    {
+        return [
+            'AUTO' => __('Automatic'),
+            '1'    => 1,
+            '2'    => 2,
+        ];
+    }
+}
