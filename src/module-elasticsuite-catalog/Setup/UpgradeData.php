01a37ad21f8d598e078e912f605a74f69b9db72f
Romain Ruaud <romain.ruaud@smile.fr>
2016-09-15 18:38:02 +0200

    Categories autocomplete feature implementation.

---
+++ src/module-elasticsuite-catalog/Setup/UpgradeData.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalog
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteCatalog\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+/**
+ * Catalog Data Upgrade
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCatalog
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var EavSetup
+     */
+    private $eavSetup;
+
+    /**
+     * Class Constructor
+     *
+     * @param EavSetupFactory $eavSetupFactory Eav setup factory.
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * Upgrade the module data.
+     *
+     * @param ModuleDataSetupInterface $setup   The setup interface
+     * @param ModuleContextInterface   $context The module Context
+     *
+     * @return void
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        $this->eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        if (version_compare($context->getVersion(), '1.2.0', '<')) {
+            $this->updateCategorySearchableAttributes();
+        }
+
+        $setup->endSetup();
+    }
+
+    /**
+     * Update some categories attributes to have them indexed into ES.
+     * Basically :
+     *  - Name (indexable and searchable
+     *  - Description (indexable and searchable)
+     *  - Url Path (indexable)
+     */
+    private function updateCategorySearchableAttributes()
+    {
+        $setup      = $this->eavSetup->getSetup();
+        $connection = $setup->getConnection();
+        $table      = $setup->getTable('catalog_eav_attribute');
+
+        // Set Name and description indexable and searchable.
+        $attributeIds = [
+            $this->eavSetup->getAttributeId(\Magento\Catalog\Model\Category::ENTITY, 'name'),
+            $this->eavSetup->getAttributeId(\Magento\Catalog\Model\Category::ENTITY, 'description'),
+        ];
+
+        foreach (['is_searchable', 'is_used_in_spellcheck'] as $configField) {
+            foreach ($attributeIds as $attributeId) {
+                $connection->update(
+                    $table,
+                    [$configField => 1],
+                    $connection->quoteInto('attribute_id = ?', $attributeId)
+                );
+            }
+        }
+
+        // Set url_path indexable.
+        $urlPathAttributeId = $this->eavSetup->getAttributeId(\Magento\Catalog\Model\Category::ENTITY, 'url_path');
+        $connection->update(
+            $table,
+            ['is_searchable' => 1],
+            $connection->quoteInto('attribute_id = ?', $urlPathAttributeId)
+        );
+    }
+}
