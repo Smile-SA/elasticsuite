c2444230bb68ecc27fbd64ddec18353ce072d644
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-09-26 11:13:59 +0200

    Fix category filter inconsistent beahvior in fulltext search. Now reset others filters.

--- src/module-elasticsuite-catalog/Model/Layer/Filter/Item/Category.php
+++ src/module-elasticsuite-catalog/Model/Layer/Filter/Item/Category.php
@@ -27,7 +27,17 @@ class Category extends \Magento\Catalog\Model\Layer\Filter\Item
      */
     public function getUrl()
     {
-        $url = parent::getUrl();
+        $query = [
+            $this->getFilter()->getRequestVar() => $this->getValue(),
+            // exclude current page from urls
+            $this->_htmlPagerBlock->getPageVarName() => null,
+        ];
+
+        foreach ($this->getFilter()->getLayer()->getState()->getFilters() as $currentFilterItem) {
+            $query[$currentFilterItem->getFilter()->getRequestVar()] = null;
+        }
+
+        $url = $this->_url->getUrl('*/*/*', ['_current' => true, '_use_rewrite' => true, '_query' => $query]);
 
         if ($this->getUrlRewrite()) {
             $url = $this->getUrlRewrite();
