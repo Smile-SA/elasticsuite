19b7e1ee40186562eb331b4c83911645f3a7c891
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-10-12 10:31:24 +0200

    Improve lambda function usage and performance for attribute indexer processing.

--- src/module-elasticsuite-catalog/Helper/Attribute.php
+++ src/module-elasticsuite-catalog/Helper/Attribute.php
@@ -48,6 +48,11 @@ class Attribute extends Mapping
     private $attributeOptionTextCache = [];
 
     /**
+     * @var array
+     */
+    private $attributeMappers = [];
+
+    /**
      * Object manager
      *
      * @var ObjectManagerInterface
@@ -159,9 +164,13 @@ class Attribute extends Mapping
         $attributeCode = $attribute->getAttributeCode();
         $values = [];
 
-        $simpleValueMapper = function ($value) use ($attribute) {
-            return $this->prepareSimpleIndexAttributeValue($attribute, $value);
-        };
+        $mapperKey = 'simple_' . $attribute->getId();
+
+        if (!isset($this->attributeMappers[$mapperKey])) {
+            $this->attributeMappers[$mapperKey] = function ($value) use ($attribute) {
+                return $this->prepareSimpleIndexAttributeValue($attribute, $value);
+            };
+        }
 
         if ($attribute->usesSource() && !is_array($value)) {
             $value = explode(',', $value);
@@ -171,7 +180,7 @@ class Attribute extends Mapping
             $value = [$value];
         }
 
-        $value = array_map($simpleValueMapper, $value);
+        $value = array_map($this->attributeMappers[$mapperKey], $value);
         $value = array_filter($value);
         $value = array_values($value);
         $values[$attributeCode] = $value;
@@ -199,10 +208,15 @@ class Attribute extends Mapping
      */
     public function getIndexOptionsText(AttributeInterface $attribute, $storeId, array $optionIds)
     {
-        $mapper = function ($optionId) use ($attribute, $storeId) {
-            return $this->getIndexOptionText($attribute, $storeId, $optionId);
-        };
-        $optionValues = array_map($mapper, $optionIds);
+        $mapperKey = sprintf("options_%s_%s", $attribute->getId(), $storeId);
+
+        if (!isset($this->attributeMappers[$mapperKey])) {
+            $this->attributeMappers[$mapperKey] = function ($optionId) use ($attribute, $storeId) {
+                return $this->getIndexOptionText($attribute, $storeId, $optionId);
+            };
+        }
+
+        $optionValues = array_map($this->attributeMappers[$mapperKey], $optionIds);
 
         return $optionValues;
     }
@@ -222,11 +236,7 @@ class Attribute extends Mapping
         $attribute = $this->getAttributeByStore($attribute, $storeId);
         $attributeId = $attribute->getAttributeId();
 
-        if (!isset($this->attributeOptionTextCache[$storeId])) {
-            $this->attributeOptionTextCache[$storeId] = [];
-        }
-
-        if (!isset($this->attributeOptionTextCache[$storeId])) {
+        if (!isset($this->attributeOptionTextCache[$storeId]) || !isset($this->attributeOptionTextCache[$storeId][$attributeId])) {
             $this->attributeOptionTextCache[$storeId][$attributeId] = [];
         }
 
