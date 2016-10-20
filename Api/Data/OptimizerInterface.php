2d28e85eee7fa377794f850df8d283c987200156
Fanny DECLERCK <fadec@smile.fr>
2016-10-20 12:03:34 +0200

    Init ElasticsuiteCatalogOptimizer module and install schema table

---
+++ Api/Data/OptimizerInterface.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalogOptimizer
+ * @author    Fanny DECLERCK <fadec@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteCatalogOptimizer\Api\Data;
+
+/**
+ * Elasticsuite Catalog Optimizer Interface
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCatalogOptimizer
+ * @author   Fanny DECLERCK <fadec@smile.fr>
+ */
+/**
+ * Interface OptimizerInterface
+ * @package Smile\ElasticsuiteCatalogOptimizer\Api\Data
+ */
+interface OptimizerInterface
+{
+    /**
+     * Name of the main Mysql Table
+     */
+    const TABLE_NAME = 'smile_elasticsuite_optimizer';
+
+    /**
+     * Name of the join Mysql Table
+     */
+    const TABLE_NAME_SEARCH_CONTAINER = 'smile_elasticsuite_optimizer_search_container';
+    
+    /**
+     * Constant for field optimizer_id
+     */
+    const OPTIMIZER_ID = 'optimizer_id';
+
+    /**
+     * Constant for field name
+     */
+    const NAME = 'name';
+
+    /**
+     * Constant for field is_active
+     */
+    const IS_ACTIVE = 'is_active';
+
+    /**
+     * Constant for field model
+     */
+    const MODEL = 'model';
+
+    /**
+     * Constant for field config
+     */
+    const CONFIG = 'config';
+
+    /**
+     * Constant for field store_id
+     */
+    const STORE_ID = 'store_id';
+
+    /**
+     * Constant for field from_date
+     */
+    const FROM_DATE = 'from_date';
+
+    /**
+     * Constant for field to_date
+     */
+    const TO_DATE = 'to_date';
+
+    /**
+     * Constant for field search_container
+     */
+    const SEARCH_CONTAINER = 'search_container';
+
+    /**
+     * Get Optimizer ID
+     *
+     * @return int|null
+     */
+    public function getOptimizerId();
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Get Optimizer status
+     *
+     * @return bool
+     */
+    public function isActive();
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel();
+
+    /**
+     * Get config
+     *
+     * @return string
+     */
+    public function getConfig();
+
+    /**
+     * Get store id
+     *
+     * @return int
+     */
+    public function getStoreId();
+
+    /**
+     * Get from_date
+     *
+     * @return date
+     */
+    public function getFromDate();
+
+    /**
+     * Get to_date
+     *
+     * @return date
+     */
+    public function getToDate();
+
+    /**
+     * Set name
+     *
+     * @param string $name the value to save
+     *
+     * @return OptimizerInterface
+     */
+    public function setName($name);
+
+    /**
+     * Set Optimizer status
+     *
+     * @param bool $status The optimizer status
+     *
+     * @return OptimizerInterface
+     */
+    public function setIsActive($status);
+
+    /**
+     * Set model
+     *
+     * @param string $model The model of optimizer to save
+     *
+     * @return OptimizerInterface
+     */
+    public function setModel($model);
+
+    /**
+     * Set config
+     *
+     * @param string $config The config of optimizer to save
+     *
+     * @return OptimizerInterface
+     */
+    public function setConfig($config);
+
+    /**
+     * Set store id
+     *
+     * @param int $storeId the store id
+     *
+     * @return OptimizerInterface
+     */
+    public function setStoreId($storeId);
+
+    /**
+     * Set from_date
+     *
+     * @param date $fromDate The from date.
+     *
+     * @return OptimizerInterface
+     */
+    public function setFromDate($fromDate);
+
+    /**
+     * Set to_date
+     *
+     * @param date $toDate The to date
+     *
+     * @return OptimizerInterface
+     */
+    public function setToDate($toDate);
+
+    /**
+     * Get search_container
+     *
+     * @return string
+     */
+    public function getSearchContainer();
+
+    /**
+     * Set search container.
+     *
+     * @param string $searchContainer The value to search container.
+     *
+     * @return OptimizerInterface
+     */
+    public function setSearchContainer($searchContainer);
+
+}
---
+++ Setup/InstallSchema.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalogOptimizer
+ * @author    Fanny DECLERCK <fadec@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteCatalogOptimizer\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Smile\ElasticsuiteCatalogOptimizer\Api\Data\OptimizerInterface;
+use Smile\ElasticsuiteCatalogOptimizer\Api\Data\OptimizerQueryTypeInterface;
+
+/**
+ * Install Schema for Catalog Optimizer Module
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCatalogOptimizer
+ * @author   Fanny DECLERCK <fadec@smile.fr>
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        $this->createOptimizerTable($setup);
+        $this->createOptimizerSearchContainerTable($setup);
+
+        $setup->endSetup();
+    }
+
+    /**
+     * Create Optimizer main table.
+     *
+     * @param \Magento\Framework\Setup\SchemaSetupInterface $setup Setup instance
+     */
+    private function createOptimizerTable(SchemaSetupInterface $setup)
+    {
+        if (!$setup->getConnection()->isTableExists($setup->getTable(OptimizerInterface::TABLE_NAME))) {
+            $table = $setup->getConnection()
+                ->newTable($setup->getTable(OptimizerInterface::TABLE_NAME))
+                ->addColumn(
+                    OptimizerInterface::OPTIMIZER_ID,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['identity' => true, 'nullable' => false, 'primary' => true],
+                    'Optimizer ID'
+                )
+                ->addColumn(
+                    OptimizerInterface::STORE_ID,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false],
+                    'Store id'
+                )
+                ->addColumn(
+                    OptimizerInterface::IS_ACTIVE,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
+                    null,
+                    ['nullable' => false, 'default' => '1'],
+                    'Is Optimizer Active'
+                )
+                ->addColumn(
+                    OptimizerInterface::FROM_DATE,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_DATE,
+                    null,
+                    ['nullable' => true],
+                    'Enable rule from date'
+                )
+                ->addColumn(
+                    OptimizerInterface::TO_DATE,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_DATE,
+                    null,
+                    ['nullable' => true],
+                    'Enable rule to date'
+                )
+                ->addColumn(
+                    OptimizerInterface::NAME,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    ['nullable' => false],
+                    'Optimizer Name'
+                )
+                ->addColumn(
+                    OptimizerInterface::MODEL,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'Optimizer model'
+                )
+                ->addColumn(
+                    OptimizerInterface::CONFIG,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    '2M',
+                    [],
+                    'Optimizer serialized configuration'
+                )
+                ->setComment('Search optimizer Table');
+
+            $setup->getConnection()->createTable($table);
+        }
+    }
+
+    /**
+     * Create Optimizer Query table.
+     *
+     * @param \Magento\Framework\Setup\SchemaSetupInterface $setup Setup instance
+     */
+    private function createOptimizerSearchContainerTable(SchemaSetupInterface $setup)
+    {
+        if (!$setup->getConnection()->isTableExists($setup->getTable(OptimizerInterface::TABLE_NAME_SEARCH_CONTAINER))) {
+            $table = $setup->getConnection()
+                ->newTable($setup->getTable(OptimizerInterface::TABLE_NAME_SEARCH_CONTAINER))
+                ->addColumn(
+                    OptimizerInterface::OPTIMIZER_ID,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false, 'primary' => true],
+                    'Optimizer ID'
+                )
+                ->addColumn(
+                    OptimizerInterface::SEARCH_CONTAINER,
+                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    255,
+                    ['nullable' => false, 'primary' => true],
+                    'Search Container'
+                )
+                ->addIndex(
+                    $setup->getIdxName(OptimizerInterface::TABLE_NAME, [OptimizerInterface::SEARCH_CONTAINER]),
+                    [OptimizerInterface::SEARCH_CONTAINER]
+                )
+                ->addForeignKey(
+                    $setup->getFkName(
+                        OptimizerInterface::TABLE_NAME_SEARCH_CONTAINER,
+                        OptimizerInterface::OPTIMIZER_ID,
+                        OptimizerInterface::TABLE_NAME,
+                        OptimizerInterface::OPTIMIZER_ID
+                    ),
+                    OptimizerInterface::OPTIMIZER_ID,
+                    $setup->getTable(OptimizerInterface::TABLE_NAME),
+                    OptimizerInterface::OPTIMIZER_ID,
+                    \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+                )
+                ->setComment('Query type per optimizer table');
+
+            $setup->getConnection()->createTable($table);
+        }
+    }
+}
