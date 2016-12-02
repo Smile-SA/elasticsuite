1cafe5dd6e14c16471a8c280fdab7aba03aaacd8
Romain Ruaud <romain.ruaud@smile.fr>
2016-12-02 14:34:32 +0100

    Fixing code style on product attributes renderer.

--- src/module-elasticsuite-catalog/view/frontend/web/js/autocomplete/product-attribute.js
+++ src/module-elasticsuite-catalog/view/frontend/web/js/autocomplete/product-attribute.js
@@ -1,8 +1,25 @@
-define(['underscore'], function(_) {
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalog
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+/*jshint browser:true jquery:true*/
+/*global alert*/
+
+define(['underscore'], function(_) {
     var Renderer = {
         render : function (data) {
-            var data = data.filter(function(item) {
-                return item.type == "product_attribute"; 
+            data = data.filter(function(item) {
+                return item.type === "product_attribute";
             }).map(function(item) {
                 return item['attribute_label']
             }).reduce(function(prev, item) {
@@ -16,7 +33,7 @@ define(['underscore'], function(_) {
 
             data = _.pairs(data).sort(function(item1, item2) {
                 return item2[0] - item1[0]
-            }).map(function(item) {return item[0]});
+            }).map(function(item) {return item[0]});
 
             if (data.length > 2) {
                 data = data.slice(0, 2);
@@ -26,5 +43,6 @@ define(['underscore'], function(_) {
             return data.join(', ');
         }
     }
+
     return Renderer;
 });
