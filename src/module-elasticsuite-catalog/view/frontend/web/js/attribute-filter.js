178672a96e8b1d8990f3a26618f472a623053b32
Romain Ruaud <romain.ruaud@smile.fr>
2016-12-02 14:18:53 +0100

    Lower complexity of attribute filter rendering.

--- src/module-elasticsuite-catalog/view/frontend/web/js/attribute-filter.js
+++ src/module-elasticsuite-catalog/view/frontend/web/js/attribute-filter.js
@@ -23,9 +23,12 @@ define([
         defaults: {
             template: "Smile_ElasticsuiteCatalog/attribute-filter",
             showMoreLabel: $.mage.__("Show more"),
-            showLessLabel: $.mage.__("Show less"),
+            showLessLabel: $.mage.__("Show less")
         },
 
+        /**
+         * Component initialization
+         */
         initialize: function () {
             this._super();
             this.expanded = false;
@@ -41,6 +44,9 @@ define([
             this.onShowLess();
         },
 
+        /**
+         * Init the place holder
+         */
         initSearchPlaceholder: function () {
             var examples = this.items.slice(0, 2).map(function (item) {return item.label});
 
@@ -50,38 +56,51 @@ define([
 
             this.searchPlaceholder = $.mage.__('Search (%s)').replace('%s', examples.join(', '));
         },
-        
+
+        /**
+         * Triggered when typing on the search input
+         */
         onSearchChange: function () {
-            if (this.fulltextSearch().trim() == "") {
-                this.items = this.items;
+            if (this.fulltextSearch().trim() === "") {
                 this.fulltextSearch(null);
                 this.onShowLess();
-            } else {
-
             }
         },
-        
+
+        /**
+         * Retrieve additional Results
+         *
+         * @param callback
+         */
         loadAdditionalItems: function (callback) {
             $.get(this.ajaxLoadUrl, function (data) {
                 this.items = data;
                 this.hasMoreItems  = false;
                 
                 if (callback) {
-                    callback();
+                    return callback();
                 }
             }.bind(this));
         },
-        
+
+        /**
+         * Retrieve items to display
+         *
+         * @returns {*}
+         */
         getDisplayedItems: function () {
             var items = this.items;
             
-            if (this.expanded() == false) {
+            if (this.expanded() === false) {
                 items = this.items.slice(0, this.maxSize);
             }
             
             return items;
         },
-        
+
+        /**
+         * Callback for the "Show more" button
+         */
         onShowMore: function () {
             if (this.hasMoreItems) {
                 this.loadAdditionalItems(this.onShowMore.bind(this));
@@ -89,21 +108,39 @@ define([
                 this.expanded(true);
             }
         },
-        
+
+        /**
+         * Callback for the "Show less" button
+         */
         onShowLess: function () {
             this.expanded(false);
         },
-        
+
+        /**
+         * Check if the filter can be expanded
+         *
+         * @returns {boolean}
+         */
         enableExpansion : function () {
            return this.hasMoreItems || this.items.length > this.maxSize;
         },
 
+        /**
+         * Displays the "Show More" link
+         *
+         * @returns {*|boolean}
+         */
         displayShowMore: function () {
-            return this.enableExpansion() && this.expanded() == false && this.fulltextSearch() == null;
+            return this.enableExpansion() && this.expanded() === false && !this.fulltextSearch();
         },
-        
+
+        /**
+         * Displays the "Show Less" link
+         *
+         * @returns {*|boolean}
+         */
         displayShowLess: function () {
-            return this.enableExpansion() && this.expanded() == true && this.fulltextSearch() == null;
+            return this.enableExpansion() && this.expanded() === true && !this.fulltextSearch();
         }
     });
 });
