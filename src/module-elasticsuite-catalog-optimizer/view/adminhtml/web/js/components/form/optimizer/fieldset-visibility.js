cc70637a6bd8aae68138d65dc3dd8006231f7bfa
Romain Ruaud <romain.ruaud@smile.fr>
2016-12-12 17:18:27 +0100

    Fixing file headers.

--- src/module-elasticsuite-catalog-optimizer/view/adminhtml/web/js/components/form/optimizer/fieldset-visibility.js
+++ src/module-elasticsuite-catalog-optimizer/view/adminhtml/web/js/components/form/optimizer/fieldset-visibility.js
@@ -1,3 +1,17 @@
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalogOptimizer
+ * @author    Fanny DECLERCK <fadec@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
 define(['Magento_Ui/js/form/components/fieldset'], function(Fieldset) {
     return Fieldset.extend({
         defaults: {
