9dc69ae84556b87b953a582241a7f748a7039683
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2017-01-12 14:45:07 +0100

    Search adapter query refactoring for better extensibility using DI.

--- src/module-elasticsuite-core/Search/Adapter/Elasticsuite/Request/Query/Builder.php
+++ src/module-elasticsuite-core/Search/Adapter/Elasticsuite/Request/Query/Builder.php
@@ -15,7 +15,6 @@
 namespace Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query;
 
 use Smile\ElasticsuiteCore\Search\Request\QueryInterface;
-use Magento\Framework\ObjectManagerInterface;
 
 /**
  * Build Elasticsearch queries from search request QueryInterface queries.
@@ -27,36 +26,18 @@ use Magento\Framework\ObjectManagerInterface;
 class Builder implements BuilderInterface
 {
     /**
-     * @var array
+     * @var BuilderInterface[]
      */
-    private $queryBuilderClasses = [
-        QueryInterface::TYPE_BOOL          => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Boolean',
-        QueryInterface::TYPE_FILTER        => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Filtered',
-        QueryInterface::TYPE_NOT           => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Not',
-        QueryInterface::TYPE_NESTED        => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Nested',
-        QueryInterface::TYPE_TERM          => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Term',
-        QueryInterface::TYPE_TERMS         => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Terms',
-        QueryInterface::TYPE_RANGE         => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Range',
-        QueryInterface::TYPE_MATCH         => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Match',
-        QueryInterface::TYPE_COMMON        => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Common',
-        QueryInterface::TYPE_MULTIMATCH    => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\MultiMatch',
-        QueryInterface::TYPE_MISSING       => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Missing',
-        QueryInterface::TYPE_FUNCTIONSCORE => 'Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\FunctionScore',
-    ];
-
-    /**
-     * @var ObjectManagerInterface
-     */
-    private $objectManager;
+    private $builders;
 
     /**
      * Constructor.
      *
-     * @param ObjectManagerInterface $objectManager Object Manager.
+     * @param BuilderInterface[] $builders Builders implementations.
      */
-    public function __construct(ObjectManagerInterface $objectManager)
+    public function __construct(array $builders)
     {
-        $this->objectManager = $objectManager;
+        $this->builders = $builders;
     }
 
     /**
@@ -69,7 +50,7 @@ class Builder implements BuilderInterface
     public function buildQuery(QueryInterface $query)
     {
         $searchQuery = false;
-        $builder = $this->getBuilder($query);
+        $builder     = $this->getBuilder($query);
 
         if ($builder !== null) {
             $searchQuery = $builder->buildQuery($query);
@@ -83,18 +64,16 @@ class Builder implements BuilderInterface
      *
      * @param QueryInterface $query Query to be built.
      *
-     * @return BuilderInterface|null
+     * @return BuilderInterface
      */
     private function getBuilder($query)
     {
-        $builder = null;
         $queryType = $query->getType();
 
-        if (isset($this->queryBuilderClasses[$queryType])) {
-            $builderClass = $this->queryBuilderClasses[$queryType];
-            $builder = $this->objectManager->get($builderClass, ['builder' => $this]);
+        if (!isset($this->builders[$queryType])) {
+            throw new \InvalidArgumentException("Unknow query builder for {$queryType}.");
         }
 
-        return $builder;
+        return $this->builders[$queryType];
     }
 }
