d701f4baaa674adf11a0335ed9146138e629425e
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2017-04-25 14:49:59 +0200

    Refactoring product autocomplete.

---
+++ src/module-elasticsuite-catalog/Model/Autocomplete/Product/Collection/AttributeSelector.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalog
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\Collection;
+
+use Smile\ElasticsuiteCatalog\Model\ResourceModel\Product\Fulltext\Collection as ProductCollection;
+use Smile\ElasticsuiteCatalog\Model\Autocomplete\Product\AttributeConfig;
+
+/**
+ * Catalog autocomplete product collection attributes.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCatalog
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class AttributeSelector implements PreProcessorInterface
+{
+    /**
+     * @var string[]
+     */
+    private $attributes;
+
+    /**
+     * Constructor.
+     *
+     * @param AttributeConfig $selectedAttributes Attribute selected.
+     */
+    public function __construct(AttributeConfig $selectedAttributes)
+    {
+        $this->attributes = $selectedAttributes->getSelectedAttributeCodes();
+    }
+
+    /**
+     * Add standard attributes and price data to the collection.
+     *
+     * @param ProductCollection $collection Product collection.
+     *
+     * @return ProductCollection
+     */
+    public function prepareCollection(ProductCollection $collection)
+    {
+        $collection->addAttributeToSelect($this->attributes);
+        $collection->addPriceData();
+
+        return $collection;
+    }
+}
