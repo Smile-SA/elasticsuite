2536b7737d6ef27a628f0dee53ab2413be97045d
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2017-11-27 14:19:48 +0100

    Agg : Date histogram bucket support.

---
+++ src/module-elasticsuite-core/Search/Request/Aggregation/Bucket/DateHistogram.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCore
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticsuiteCore\Search\Request\Aggregation\Bucket;
+
+use Smile\ElasticsuiteCore\Search\Request\BucketInterface;
+use Smile\ElasticsuiteCore\Search\Request\QueryInterface;
+
+/**
+ * Date historgram bucket implementation.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCore
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class DateHistogram extends Histogram
+{
+    public function __construct(
+        $name,
+        $field,
+        array $metrics,
+        array $childBuckets = [],
+        $nestedPath = null,
+        QueryInterface $filter = null,
+        QueryInterface $nestedFilter = null,
+        $interval = "1d",
+        $minDocCount = 0
+    ) {
+        parent::__construct($name, $field, $metrics, $childBuckets, $nestedPath, $filter, $nestedFilter, $interval, $minDocCount);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getType()
+    {
+        return BucketInterface::TYPE_DATE_HISTOGRAM;
+    }
+}
