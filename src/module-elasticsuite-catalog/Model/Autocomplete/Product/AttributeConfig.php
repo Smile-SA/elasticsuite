58f4a05a44d1d0c06a8cf124cbbbf9142a590a79
Romain Ruaud <romain.ruaud@smile.fr>
2018-01-17 14:30:20 +0100

    Append special_from_date and special_to_date attributes.

--- src/module-elasticsuite-catalog/Model/Autocomplete/Product/AttributeConfig.php
+++ src/module-elasticsuite-catalog/Model/Autocomplete/Product/AttributeConfig.php
@@ -32,7 +32,7 @@ class AttributeConfig
     /**
      * @var string[]
      */
-    private $defaultSelectedAttributes = ['name', 'thumbnail', 'special_price'];
+    private $defaultSelectedAttributes = ['name', 'thumbnail', 'special_price', 'special_from_date', 'special_to_date'];
 
     /**
      * @var string[]
