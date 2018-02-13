66ff5d5a3294d0a95bb9bcbb71a4f17ab3ec24dc
Romain Ruaud <romain.ruaud@smile.fr>
2018-02-13 15:59:14 +0100

    Add 'is_blacklisted' column into Search position table.

--- src/module-elasticsuite-catalog/Setup/UpgradeSchema.php
+++ src/module-elasticsuite-catalog/Setup/UpgradeSchema.php
@@ -70,6 +70,10 @@ class UpgradeSchema implements UpgradeSchemaInterface
             $this->catalogSetup->createSearchPositionTable($setup);
         }
 
+        if (version_compare($context->getVersion(), '1.5.0', '<')) {
+            $this->catalogSetup->addBlacklistColumnToSearchPositionTable($setup);
+        }
+
         $setup->endSetup();
     }
 }
