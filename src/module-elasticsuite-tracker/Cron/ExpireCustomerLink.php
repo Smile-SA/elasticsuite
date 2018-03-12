fe24f7bcb5a1974b2f45354af30c811a99846a49
Romain Ruaud <romain.ruaud@smile.fr>
2018-03-12 14:34:31 +0100

    Implementation of customer link service with a dedicated table.
    Cleanup tracker data.
    
    Add service to save customer reconciliation.
    
    Add module dependencies.
    
    Adding configurable delay for anonymization and a cronjob to clean it once a day.
    
    Refactoring reconciliation service to tracking service.
    
    Adding getVisitorIds implementation.
    
    Rename reconciliation to CustomerLink

---
+++ src/module-elasticsuite-tracker/Cron/ExpireCustomerLink.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteTracker
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2017 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteTracker\Cron;
+
+/**
+ * Cronjob that will anonymize log events that have reached their "delete_after" date if the anonymization is enabled.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteTracker
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class ExpireCustomerLink
+{
+    /**
+     * @var \Smile\ElasticsuiteTracker\Api\CustomerTrackingServiceInterface
+     */
+    private $trackingService;
+
+    /**
+     * @var \Smile\ElasticsuiteTracker\Helper\Data
+     */
+    private $helper;
+
+    /**
+     * Constructor.
+     *
+     * @param \Smile\ElasticsuiteTracker\Api\CustomerTrackingServiceInterface $trackingService Tracking service.
+     * @param \Smile\ElasticsuiteTracker\Helper\Data                          $helper          Tracking helper.
+     */
+    public function __construct(
+        \Smile\ElasticsuiteTracker\Api\CustomerTrackingServiceInterface $trackingService,
+        \Smile\ElasticsuiteTracker\Helper\Data $helper
+    ) {
+        $this->trackingService = $trackingService;
+        $this->helper          = $helper;
+    }
+
+    /**
+     * Perform the cleaning of the expired entries if necessary and if anonymization is enabled.
+     */
+    public function execute()
+    {
+        if ($this->helper->isAnonymizationEnabled()) {
+            $this->trackingService->deleteExpired();
+        }
+    }
+}
