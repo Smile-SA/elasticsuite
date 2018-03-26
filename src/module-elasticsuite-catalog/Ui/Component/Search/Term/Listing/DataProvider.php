3979f6c2ae5989e99f8dc603f2d1ff757ce9f53a
Romain Ruaud <romain.ruaud@smile.fr>
2018-03-26 16:00:59 +0200

    Adding conditional fieldsets for optimizers.

---
+++ src/module-elasticsuite-catalog/Ui/Component/Search/Term/Listing/DataProvider.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalog
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2018 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteCatalog\Ui\Component\Search\Term\Listing;
+
+/**
+ * Search terms listing data provider.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCatalog
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * Search term collection
+     *
+     * @var \Magento\Search\Model\ResourceModel\Query\Collection
+     */
+    protected $collection;
+
+    /**
+     * @var \Magento\Ui\DataProvider\AddFieldToCollectionInterface[]
+     */
+    protected $addFieldStrategies;
+
+    /**
+     * @var \Magento\Ui\DataProvider\AddFilterToCollectionInterface[]
+     */
+    protected $addFilterStrategies;
+
+    /**
+     * Construct
+     *
+     * @param string                                                      $name                Component name
+     * @param string                                                      $primaryFieldName    Primary field Name
+     * @param string                                                      $requestFieldName    Request field name
+     * @param \Magento\Search\Model\ResourceModel\Query\CollectionFactory $collectionFactory   The collection factory
+     * @param \Magento\Ui\DataProvider\AddFieldToCollectionInterface[]    $addFieldStrategies  Add field Strategy
+     * @param \Magento\Ui\DataProvider\AddFilterToCollectionInterface[]   $addFilterStrategies Add filter Strategy
+     * @param array                                                       $meta                Component Meta
+     * @param array                                                       $data                Component extra data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        $collectionFactory,
+        array $addFieldStrategies = [],
+        array $addFilterStrategies = [],
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+
+        $this->collection = $collectionFactory->create();
+
+        $this->addFieldStrategies  = $addFieldStrategies;
+        $this->addFilterStrategies = $addFilterStrategies;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addField($field, $alias = null)
+    {
+        if (isset($this->addFieldStrategies[$field])) {
+            $this->addFieldStrategies[$field]->addField($this->getCollection(), $field, $alias);
+
+            return;
+        }
+
+        parent::addField($field, $alias);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addFilter(\Magento\Framework\Api\Filter $filter)
+    {
+        if (isset($this->addFilterStrategies[$filter->getField()])) {
+            $this->addFilterStrategies[$filter->getField()]
+                ->addFilter(
+                    $this->getCollection(),
+                    $filter->getField(),
+                    [$filter->getConditionType() => $filter->getValue()]
+                );
+
+            return;
+        }
+
+        parent::addFilter($filter);
+    }
+}
