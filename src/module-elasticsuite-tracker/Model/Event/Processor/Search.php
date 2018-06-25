e2ea49a683d1b556b97ca8f8d2b229ce57cb1247
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2018-06-25 11:04:41 +0200

    Fix boolean values in search query tracking.

---
+++ src/module-elasticsuite-tracker/Model/Event/Processor/Search.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Searchandising Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteTracker
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2018 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticsuiteTracker\Model\Event\Processor;
+
+use Smile\ElasticsuiteTracker\Api\EventProcessorInterface;
+
+/**
+ * Process search query logged events.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteTracker
+ * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ */
+class Search implements EventProcessorInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process($eventData)
+    {
+        if (isset($eventData['page']['search']['is_spellchecked'])) {
+            $eventData['page']['search']['is_spellchecked'] = (bool) $eventData['page']['search']['is_spellchecked'];
+        }
+
+        return $eventData;
+    }
+}
