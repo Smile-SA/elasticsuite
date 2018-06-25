0e15aeffd1b3f21da49a4ac2c0218168877317da
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2018-06-25 11:01:14 +0200

    Fix typo in the more like this query model.

--- src/module-elasticsuite-core/Search/Request/Query/MoreLikeThis.php
+++ src/module-elasticsuite-core/Search/Request/Query/MoreLikeThis.php
@@ -235,7 +235,7 @@ class MoreLikeThis implements QueryInterface
      */
     public function getMaxDocFreq()
     {
-        return $this->minDocFreq;
+        return $this->maxDocFreq;
     }
 
     /**
