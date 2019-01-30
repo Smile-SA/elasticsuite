74a2448f75acfdbf7d85ef3b4ae9ee3ce1c609d2
Maxime LECLERCQ <maxime.leclercq@smile.fr>
2019-01-30 11:54:55 +0100

    Fix product image plugin - magento changed the visibility of getFilterArray method

--- src/module-elasticsuite-swatches/Model/Plugin/ProductImage.php
+++ src/module-elasticsuite-swatches/Model/Plugin/ProductImage.php
@@ -28,17 +28,46 @@ class ProductImage extends \Magento\Swatches\Model\Plugin\ProductImage
 {
     /**
      * {@inheritdoc}
-     * @deprecated since Magento 2.1.6
      */
-    protected function getFilterArray(array $request)
-    {
-        $filterArray = parent::getFilterArray($request);
+    public function beforeGetImage(
+        \Magento\Catalog\Block\Product\AbstractProduct $subject,
+        \Magento\Catalog\Model\Product $product,
+        $location,
+        array $attributes = []
+    ) {
+        if ($product->getTypeId() == \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE
+            && ($location == self::CATEGORY_PAGE_GRID_LOCATION || $location == self::CATEGORY_PAGE_LIST_LOCATION)) {
+            $request = $this->request->getParams();
+            if (is_array($request)) {
+                $filterArray = $this->getFilterArray($request, $product);
+                if (!empty($filterArray)) {
+                    $product = $this->loadSimpleVariation($product, $filterArray);
+                }
+            }
+        }
 
-        $attributeCodes = $this->eavConfig->getEntityAttributeCodes(\Magento\Catalog\Model\Product::ENTITY);
+        return [$product, $location, $attributes];
+    }
+
+    /**
+     * Get filters from request and replace labels by option ids.
+     *
+     * @param array                          $request Request parameters.
+     * @param \Magento\Catalog\Model\Product $product Product.
+     *
+     * @return array
+     */
+    private function getFilterArray(array $request, \Magento\Catalog\Model\Product $product)
+    {
+        $filterArray = [];
+        $attributes = $this->eavConfig->getEntityAttributes(\Magento\Catalog\Model\Product::ENTITY, $product);
 
         foreach ($request as $code => $value) {
-            if (in_array($code, $attributeCodes)) {
-                $attribute = $this->eavConfig->getAttribute(\Magento\Catalog\Model\Product::ENTITY, $code);
+            if (array_key_exists($code, $attributes)) {
+                $attribute = $attributes[$code];
+                if ($this->canReplaceImageWithSwatch($attribute)) {
+                    $filterArray[$code] = $value;
+                }
 
                 if (isset($filterArray[$code]) && !is_array($filterArray[$code])) {
                     $filterArray[$code] = [$filterArray[$code]];
