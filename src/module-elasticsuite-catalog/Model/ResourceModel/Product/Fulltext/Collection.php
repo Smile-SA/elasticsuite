1622eb58bc7ecfaf26c9a48d0061360ac04e7015
Imad <33043295+abbatis@users.noreply.github.com>
2019-02-19 15:04:28 +0100

    Allow resetting the sort order to an empty array
    In a normal Magento collection you would do something like this:
    
    $collection->getSelect()->reset(Zend_Db_Select::ORDER);
    
    However, this obviously wouldn't work for elasticsuite.

--- src/module-elasticsuite-catalog/Model/ResourceModel/Product/Fulltext/Collection.php
+++ src/module-elasticsuite-catalog/Model/ResourceModel/Product/Fulltext/Collection.php
@@ -198,6 +198,18 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
     }
 
     /**
+     * Reset the sort order.
+     *
+     * @return self
+     */
+    public function resetOrder()
+    {
+        $this->_orders = [];
+
+        return $this;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function addFieldToFilter($field, $condition = null)
