36b8e7c066cb7ec29bb52896d5d7c7de6769ba24
Dmytro Androshchuk <dmand@smile.fr>
2019-10-16 11:43:13 +0300

    Fixes #1503 fix sorting in Visual Swatches attribute (#1550)
    * Fixes #1503 fix sorting in Visual Swatches attribute
    
    * Fixes #1503 add documentation to the overridden method

--- src/module-elasticsuite-swatches/Block/Navigation/Renderer/Swatches/RenderLayered.php
+++ src/module-elasticsuite-swatches/Block/Navigation/Renderer/Swatches/RenderLayered.php
@@ -15,6 +15,7 @@
 namespace Smile\ElasticsuiteSwatches\Block\Navigation\Renderer\Swatches;
 
 use Magento\Catalog\Model\Layer\Filter\Item as FilterItem;
+use Magento\Eav\Model\Entity\Attribute;
 use Magento\Eav\Model\Entity\Attribute\Option;
 
 /**
@@ -26,6 +27,57 @@ use Magento\Eav\Model\Entity\Attribute\Option;
  */
 class RenderLayered extends \Magento\Swatches\Block\LayeredNavigation\RenderLayered
 {
+    /**
+     * Override the native method to sort swatch options in the expected
+     * order (as defined in the admin attribute parameters).
+     *
+     * @return array
+     */
+    public function getSwatchData(): array
+    {
+        if (false === $this->eavAttribute instanceof Attribute) {
+            throw new \RuntimeException('Magento_Swatches: RenderLayered: Attribute has not been set.');
+        }
+
+        $attributeOptions = [];
+        // Collect parameter labels in the expected order.
+        $attributeOptionsSort = [];
+        // Build an array whose keys are the attribute option label and not option id as in the native method.
+        $sortingArr = [];
+        foreach ($this->filter->getItems() as $item) {
+            $sortingArr[] = $item['label'];
+        }
+
+        foreach ($this->eavAttribute->getOptions() as $option) {
+            if ($currentOption = $this->getFilterOption($this->filter->getItems(), $option)) {
+                /*
+                 * Built the array with the attribute options in the expected orders with the attribute option id
+                 * as keys, because it's a requirement for the getSwatchesByOptionsId helper method.
+                 */
+                $attributeOptions[$option->getLabel()] = array_merge($currentOption, ['id' => $option->getValue()]);
+            } elseif ($this->isShowEmptyResults()) {
+                $attributeOptions[$option->getLabel()] = array_merge($this->getUnusedOption($option), ['id' => $option->getValue()]);
+            }
+        }
+
+        foreach (array_merge(array_flip($sortingArr), $attributeOptions) as $item) {
+            $attributeOptionsSort[$item['id']] = $item;
+        }
+
+        $attributeOptionIds = array_keys($attributeOptionsSort);
+        $swatches = $this->swatchHelper->getSwatchesByOptionsId($attributeOptionIds);
+
+        $data = [
+            'attribute_id' => $this->eavAttribute->getId(),
+            'attribute_code' => $this->eavAttribute->getAttributeCode(),
+            'attribute_label' => $this->eavAttribute->getStoreLabel(),
+            'options' => $attributeOptionsSort,
+            'swatches' => $swatches,
+        ];
+
+        return $data;
+    }
+
     /**
      * {@inheritDoc}
      */
