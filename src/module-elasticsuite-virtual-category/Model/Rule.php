4eb1f6a754cc30028823f22f71e7b9a4f74178df
Vadym Honcharuk <vadym.honcharuk@smile-ukraine.com>
2020-12-07 10:41:57 +0200

    Fixes #1937, update conditions

--- src/module-elasticsuite-virtual-category/Model/Rule.php
+++ src/module-elasticsuite-virtual-category/Model/Rule.php
@@ -147,11 +147,13 @@ class Rule extends \Smile\ElasticsuiteCatalogRule\Model\Rule implements VirtualR
 
         if (!in_array($category->getId(), $excludedCategories)) {
             $excludedCategories[] = $category->getId();
-            if ($category->getVirtualCategoryRoot() !== null && !empty($category->getVirtualCategoryRoot())) {
+            if ($category->getIsVirtualCategory() && $category->getVirtualCategoryRoot() !== null
+                && !empty($category->getVirtualCategoryRoot())) {
                 $rootCategoryId = $category->getVirtualCategoryRoot();
                 $rootCategory = $this->categoryFactory->create()->setStoreId($this->getStoreId())->load($rootCategoryId);
+                $catId = $rootCategory->getId();
             }
-            if ((bool) $category->getIsVirtualCategory() && $category->getIsActive() && !is_null($rootCategory->getId())) {
+            if ((bool) $category->getIsVirtualCategory() && $category->getIsActive() && isset($catId)) {
                 $query = $this->getVirtualCategoryQuery($category, $excludedCategories, $category->getData('virtual_category_root'));
             } elseif ($category->getId() && $category->getIsActive()) {
                 $query = $this->getStandardCategoryQuery($category, $excludedCategories);
