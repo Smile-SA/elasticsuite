a8d0bf63e23c455ed1bf4a5de8c1ad71e5bfca30
Romain Ruaud <romain.ruaud@smile.fr>
2021-04-16 18:18:21 +0200

    WIP : Fixing graphql pagination

---
+++ src/module-elasticsuite-catalog-graph-ql/Model/Resolver/Products/DataProvider/Product/CollectionProcessor/SearchCriteriaProcessor.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade Smile Elastic Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCatalogGraphQl
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2021 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticsuiteCatalogGraphQl\Model\Resolver\Products\DataProvider\Product\CollectionProcessor;
+
+use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Product\CollectionProcessorInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface as SearchCriteriaApplier;
+use Magento\GraphQl\Model\Query\ContextInterface;
+
+/**
+ * Dummmy Search Criteria Processor.
+ * We do not need to filter again the product collection since the search engine is already doing it.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCatalogGraphQl
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class SearchCriteriaProcessor implements CollectionProcessorInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(
+        Collection $collection,
+        SearchCriteriaInterface $searchCriteria,
+        array $attributeNames,
+        ContextInterface $context = null
+    ): Collection
+    {
+        return $collection;
+    }
+}
