95c266f9e0ab4c8f7da6082e166c5fd1acdc9a63
Pierre Gauthier <pigau@smile.fr>
2021-10-08 18:11:21 +0200

    [Virtual Category] Pagebuilder Compatibility #ESP-286

--- src/module-elasticsuite-virtual-category/view/adminhtml/requirejs-config.js
+++ src/module-elasticsuite-virtual-category/view/adminhtml/requirejs-config.js
@@ -16,5 +16,12 @@ var config = {
         '*': {
             'Magento_Catalog/catalog/category/assign-products': 'Smile_ElasticsuiteVirtualCategory/js/component/catalog/category/form/assign-products'
         }
+    },
+    config: {
+        mixins: {
+            'Magento_PageBuilder/js/form/provider/conditions-data-processor': {
+                'Smile_ElasticsuiteVirtualCategory/js/form/provider/conditions-data-processor-mixin': true
+            }
+        }
     }
 };
