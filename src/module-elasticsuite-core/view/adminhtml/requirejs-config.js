d8fb4c5c6f9e8e3e830e20d7203982206648d3cd
Botis <botis@smile.fr>
2021-10-25 18:14:24 +0200

    ESP-282 [Optimizer] Refractor proportionals boost operators

---
+++ src/module-elasticsuite-core/view/adminhtml/requirejs-config.js
@@ -0,0 +1,22 @@
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCore
+ * @author    Botis <botis@smile.fr>
+ * @copyright 2021 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+var config = {
+    map: {
+        '*': {
+            EsChart: 'Smile_ElasticsuiteCore/js/lib/chart',
+        }
+    }
+};
+
+
