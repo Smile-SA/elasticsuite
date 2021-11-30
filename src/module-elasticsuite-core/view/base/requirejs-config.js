109c2bdd66230c63eb52b5198a36001e094f3f8a
Botis <botis@smile.fr>
2021-11-30 12:56:35 +0100

    [Optimizer] Fix bug on constant score slider
     * Constant score  was not saved in DB when the value was set only via the slider

---
+++ src/module-elasticsuite-core/view/base/requirejs-config.js
@@ -0,0 +1,23 @@
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCore
+ * @author    Botis <botis@smile.fr>
+ * @copyright 2021 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/lib/validation/validator': {
+                'Smile_ElasticsuiteCore/js/validation/validator-mixin': true
+            }
+        }
+    },
+};
+
