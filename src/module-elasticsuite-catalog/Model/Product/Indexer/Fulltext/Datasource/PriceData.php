9848de2cbc0febed69cf0f9e18e16e5e00f8c535
Kostadin Bashev <bashev@webcode.bg>
2022-11-02 13:54:12 +0200

    revert

--- src/module-elasticsuite-catalog/Model/Product/Indexer/Fulltext/Datasource/PriceData.php
+++ src/module-elasticsuite-catalog/Model/Product/Indexer/Fulltext/Datasource/PriceData.php
@@ -67,8 +67,6 @@ class PriceData implements DatasourceInterface
     {
         $productIds = array_keys($indexData);
         $priceData = $this->resourceModel->loadPriceData($storeId, $productIds);
-        $allChildrenIds = $this->attributeResourceModel->loadChildrens($productIds, $storeId);
-        $childPriceData = $this->resourceModel->loadPriceData($storeId, array_keys($allChildrenIds));
 
         foreach ($priceData as $priceDataRow) {
             $productId     = (int) $priceDataRow['entity_id'];
@@ -82,6 +80,8 @@ class PriceData implements DatasourceInterface
             if (in_array($productTypeId, $this->attributeResourceModel->getCompositeTypes())) {
                 $isDiscount = false;
                 $priceModifier = $this->getPriceDataReader('default');
+                $allChildrenIds = $this->attributeResourceModel->loadChildrens([$productId], $storeId);
+                $childPriceData = $this->resourceModel->loadPriceData($storeId, array_keys($allChildrenIds));
                 foreach ($childPriceData as $childPrice) {
                     if ($childPrice['customer_group_id'] == $priceDataRow['customer_group_id']) {
                         if ($priceModifier->getPrice($childPrice) < $priceModifier->getOriginalPrice($childPrice)) {
