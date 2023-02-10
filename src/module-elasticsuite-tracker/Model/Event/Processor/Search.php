ee580e2d92868e55683cd8cb155d68ccac76b2b4
Romain Ruaud <romain.ruaud@smile.fr>
2023-02-10 12:47:43 +0100

    Ensure casting of query_id for already existing indices.

--- src/module-elasticsuite-tracker/Model/Event/Processor/Search.php
+++ src/module-elasticsuite-tracker/Model/Event/Processor/Search.php
@@ -34,6 +34,13 @@ class Search implements EventProcessorInterface
             $eventData['page']['search']['is_spellchecked'] = (bool) $eventData['page']['search']['is_spellchecked'];
         }
 
+        // The query_id is an "integer" in the XML mapping, but if the index already exists, it will guess the mapping.
+        // So we need to cast as integer for the "first insert on an already existing index".
+        // Otherwise it would be mapped as a string and might cause failures when aggregating on this field.
+        if (isset($eventData['page']['search']['query_id'])) {
+            $eventData['page']['search']['query_id'] = (int) $eventData['page']['search']['query_id'];
+        }
+
         return $eventData;
     }
 }
