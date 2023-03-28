ad15c71e90823d2ec2d5a0f990a25a64d24c0a27
Romain Ruaud <romain.ruaud@smile.fr>
2023-03-28 19:42:38 +0200

    Implement span queries.

---
+++ src/module-elasticsuite-core/Search/Adapter/Elasticsuite/Request/Query/Builder/Span/SpanMultiTerm.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteCore
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2023 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\Span;
+
+use Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\Builder\AbstractComplexBuilder;
+use Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Request\Query\BuilderInterface;
+use Smile\ElasticsuiteCore\Search\Request\Query\SpanQueryInterface;
+use Smile\ElasticsuiteCore\Search\Request\QueryInterface;
+
+/**
+ * Build an ES span_multi query.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteCore
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class SpanMultiTerm extends AbstractComplexBuilder implements BuilderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function buildQuery(QueryInterface $query)
+    {
+        if ($query->getType() !== SpanQueryInterface::TYPE_SPAN_MULTI_TERM) {
+            throw new \InvalidArgumentException("Query builder : invalid query type {$query->getType()}");
+        }
+
+        return [
+            'span_multi' => [
+                'boost' => $query->getBoost(),
+                'match' => $this->parentBuilder->buildQuery($query->getMatch()),
+            ],
+        ];
+    }
+}
