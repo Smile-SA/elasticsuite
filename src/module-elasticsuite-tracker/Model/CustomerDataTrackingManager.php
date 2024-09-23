a3f62da2e10fd10959e9f57b924e24e8a860b16a
Romain Ruaud <romain.ruaud@smile.fr>
2024-09-23 13:18:52 +0200

    Track customer data with JS instead of backend.

--- src/module-elasticsuite-tracker/Model/CustomerDataTrackingManager.php
+++ src/module-elasticsuite-tracker/Model/CustomerDataTrackingManager.php
@@ -14,10 +14,14 @@
 namespace Smile\ElasticsuiteTracker\Model;
 
 use DateTime;
+use Magento\Framework\Module\Manager as ModuleManager;
 use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 /**
- * Tracking Indices Manager
+ * Additional customer data tracking
  *
  * @category Smile
  * @package  Smile\ElasticsuiteTracker
@@ -31,11 +35,30 @@ class CustomerDataTrackingManager
     protected $customerSession;
 
     /**
+     * @var \Magento\Framework\Module\Manager
+     */
+    private $moduleManager;
+
+    /**
+     * @var \Magento\Company\Api\CompanyRepositoryInterface|null
+     */
+    private $companyRepository = null;
+
+    /**
      * @param CustomerSession $customerSession Customer session
      */
-    public function __construct(CustomerSession $customerSession)
+    public function __construct(CustomerSession $customerSession/*, ModuleManager $moduleManager, ObjectManager $objectManager*/)
     {
         $this->customerSession = $customerSession;
+        //$this->moduleManager   = $moduleManager;
+        // Check if Magento_Company module is enabled before attempting to load the repository.
+        /*if ($this->moduleManager->isEnabled('Magento_Company')) {
+            if (interface_exists('\Magento\Company\Api\CompanyRepositoryInterface')) {
+                $this->companyRepository = $objectManager->get(\Magento\Company\Api\CompanyRepositoryInterface::class);
+            } else {
+                throw new LocalizedException(__('CompanyRepositoryInterface is not available.'));
+            }
+        }*/
     }
 
     /**
@@ -55,12 +78,28 @@ class CustomerDataTrackingManager
         $dob = new DateTime($customer->getDob() ?? '');
         $now = new DateTime();
 
-        return [
-            'age' => (int) $now->format('Y') - (int) $dob->format('Y'),
-            'gender' => $customer->getGender(),
-            'zipcode' => $shippingAddress ? $shippingAddress->getPostcode() : '',
-            'state' => $shippingAddress ? $shippingAddress->getRegion() : '',
-            'country' => $shippingAddress ? $shippingAddress->getCountry() : '',
+        $variables = [
+            'age'        => (int) $now->format('Y') - (int) $dob->format('Y'),
+            'gender'     => $customer->getGender(),
+            'zipcode'    => $shippingAddress ? $shippingAddress->getPostcode() : '',
+            'state'      => $shippingAddress ? $shippingAddress->getRegion() : '',
+            'country'    => $shippingAddress ? $shippingAddress->getCountry() : '',
+            'group_id'   => (int) $customer->getGroupId(),
         ];
+
+        // Check if the customer is logged in and Magento_Company is enabled.
+        if ($this->customerSession->isLoggedIn() && (null !== $this->companyRepository)) {
+            $customer = $this->customerSession->getCustomer();
+
+            try {
+                // Retrieve company information by customer ID.
+                $company = $this->companyRepository->getByCustomerId($customer->getId());
+                $variables['company_id'] = (int) $company->getId();
+            } catch (NoSuchEntityException $e) {
+                // No company found for this customer.
+            }
+        }
+
+        return $variables;
     }
 }
