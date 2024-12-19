5d30e259617e9d973b7a86f3961b7b4fe0ea5c26
Richard BAYET <richard.bayet@smile.fr>
2024-12-19 18:08:58 +0100

    [Analytics] Date picker vs switchers (store, customer group, company)

--- src/module-elasticsuite-analytics/view/adminhtml/web/js/report/customer-company-selector.js
+++ src/module-elasticsuite-analytics/view/adminhtml/web/js/report/customer-company-selector.js
@@ -12,31 +12,16 @@
  */
 
 define('Smile_ElasticsuiteAnalytics/js/report/customer-company-selector', [
-    'jquery',
-    'mage/url'
-], function($, urlBuilder) {
+    'jquery'
+], function($) {
     'use strict';
 
-    return function() {
-        // On document ready, set the selected value in the company dropdown.
-        $(document).ready(function() {
-            var urlParams = new URLSearchParams(window.location.search);
-            var selectedCompany = urlParams.get('company_id');
-
-            if (selectedCompany) {
-                $('#company_id').val(selectedCompany);
-            }
-        });
-
+    return function(config) {
         // Handle the company dropdown value change.
         $('#company_id').on('change', function() {
-            var selectedCompany = $(this).val();
-            var newUrl = new URL(window.location.href);
+            let selectedCompany = $(this).val();
 
-            newUrl.searchParams.set('company_id', selectedCompany);
-
-            // Redirect to the new URL with the company filter.
-            window.location.href = newUrl.href;
+            window.location = config.baseUrl.replace('__company_id__', selectedCompany);
         });
     };
 });
