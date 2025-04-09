b2621d0e7d044974bcc0073b53e93b6f56be150a
Romain Ruaud <romain.ruaud@smile.fr>
2025-04-09 13:42:01 +0200

    Prevent compilation errors when MSI modules are disabled.

--- src/module-elasticsuite-catalog/Model/ResourceModel/Product/Indexer/Fulltext/Datasource/InventoryData.php
+++ src/module-elasticsuite-catalog/Model/ResourceModel/Product/Indexer/Fulltext/Datasource/InventoryData.php
@@ -13,15 +13,12 @@
  */
 namespace Smile\ElasticsuiteCatalog\Model\ResourceModel\Product\Indexer\Fulltext\Datasource;
 
+use Magento\Framework\ObjectManagerInterface;
 use Smile\ElasticsuiteCatalog\Model\ResourceModel\Eav\Indexer\Indexer;
 use Smile\ElasticsuiteCatalog\Model\ResourceModel\Product\Indexer\Fulltext\Datasource\InventoryDataInterface;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Store\Model\StoreManagerInterface;
-use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
-use Magento\InventorySalesApi\Api\StockResolverInterface;
-use Magento\InventoryIndexer\Model\StockIndexTableNameResolverInterface;
-use Magento\InventoryIndexer\Indexer\IndexStructure;
 
 /**
  * Multi Source Inventory Catalog Inventory Data source resource model
@@ -33,12 +30,12 @@ use Magento\InventoryIndexer\Indexer\IndexStructure;
 class InventoryData extends Indexer implements InventoryDataInterface
 {
     /**
-     * @var StockResolverInterface
+     * @var \Magento\InventorySalesApi\Api\StockResolverInterface
      */
     private $stockResolver;
 
     /**
-     * @var StockIndexTableNameResolverInterface
+     * @var \Magento\InventoryIndexer\Model\StockIndexTableNameResolverInterface
      */
     private $stockIndexTableProvider;
 
@@ -48,23 +45,25 @@ class InventoryData extends Indexer implements InventoryDataInterface
     private $stockIdByWebsite = [];
 
     /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
      * InventoryData constructor.
      *
-     * @param ResourceConnection                   $resource                Database adapter.
-     * @param StoreManagerInterface                $storeManager            Store manager.
-     * @param MetadataPool                         $metadataPool            Metadata Pool
-     * @param StockResolverInterface               $stockResolver           Stock resolver.
-     * @param StockIndexTableNameResolverInterface $stockIndexTableProvider Stock index table provider.
+     * @param ResourceConnection     $resource      Database adapter.
+     * @param StoreManagerInterface  $storeManager  Store manager.
+     * @param MetadataPool           $metadataPool  Metadata Pool
+     * @param ObjectManagerInterface $objectManager Object Manager.
      */
     public function __construct(
         ResourceConnection $resource,
         StoreManagerInterface $storeManager,
         MetadataPool $metadataPool,
-        StockResolverInterface $stockResolver,
-        StockIndexTableNameResolverInterface $stockIndexTableProvider
+        ObjectManagerInterface $objectManager
     ) {
-        $this->stockResolver = $stockResolver;
-        $this->stockIndexTableProvider = $stockIndexTableProvider;
+        $this->objectManager = $objectManager;
 
         parent::__construct($resource, $storeManager, $metadataPool);
     }
@@ -82,17 +81,17 @@ class InventoryData extends Indexer implements InventoryDataInterface
     {
         $websiteId = $this->getWebsiteId($storeId);
         $stockId   = $this->getStockId($websiteId);
-        $tableName = $this->stockIndexTableProvider->execute($stockId);
+        $tableName = $this->getStockIndexTableProvider()->execute($stockId);
 
         $select = $this->getConnection()->select()
             ->from(['product' => $this->getTable('catalog_product_entity')], [])
             ->join(
                 ['stock_index' => $tableName],
-                'product.sku = stock_index.' . IndexStructure::SKU,
+                'product.sku = stock_index.' . \Magento\InventoryIndexer\Indexer\IndexStructure::SKU,
                 [
                     'product_id'    => 'product.entity_id',
-                    'stock_status'  => 'stock_index.' . IndexStructure::IS_SALABLE,
-                    'qty'           => 'stock_index.' . IndexStructure::QUANTITY,
+                    'stock_status'  => 'stock_index.' . \Magento\InventoryIndexer\Indexer\IndexStructure::IS_SALABLE,
+                    'qty'           => 'stock_index.' . \Magento\InventoryIndexer\Indexer\IndexStructure::QUANTITY,
                 ]
             )
             ->where('product.entity_id IN (?)', $productIds)
@@ -112,7 +111,10 @@ class InventoryData extends Indexer implements InventoryDataInterface
     {
         if (!isset($this->stockIdByWebsite[$websiteId])) {
             $websiteCode = $this->storeManager->getWebsite($websiteId)->getCode();
-            $stock = $this->stockResolver->execute(SalesChannelInterface::TYPE_WEBSITE, $websiteCode);
+            $stock = $this->getStockResolver()->execute(
+                \Magento\InventorySalesApi\Api\Data\SalesChannelInterface::TYPE_WEBSITE,
+                $websiteCode
+            );
             $stockId = (int) $stock->getStockId();
             $this->stockIdByWebsite[$websiteId] = $stockId;
         }
@@ -130,5 +132,52 @@ class InventoryData extends Indexer implements InventoryDataInterface
     private function getWebsiteId($storeId)
     {
         return $this->storeManager->getStore($storeId)->getWebsiteId();
+    }
+
+    /**
+     * Fetch the Stock Resolver from Object Manager instead of constructor to avoid compilation error when MSI modules are not there.
+     * The fact that the class exists is normally already checked in the caller class.
+     * @see Smile\ElasticsuiteCatalog\Model\Product\Indexer\Fulltext\Datasource\InventoryData
+     *
+     * @return \Magento\InventorySalesApi\Api\StockResolverInterface|null
+     *
+     * @throws \Magento\Framework\Exception\RuntimeException
+     */
+    private function getStockResolver()
+    {
+        if (null === $this->stockResolver) {
+            try {
+                $this->stockResolver = $this->objectManager->get(\Magento\InventorySalesApi\Api\StockResolverInterface::class);
+            } catch (\Exception $exception) {
+                $message = 'Failed to fetch the MSI stock resolver despite the fact MSI implementation was considered as usable. ';
+                throw new \Magento\Framework\Exception\RuntimeException(__($message . $exception->getMessage()));
+            }
+        }
+
+        return $this->stockResolver;
+    }
+
+    /**
+     * Fetch the Stock Index Table Provider from Object Manager instead of constructor
+     * to avoid compilation error when MSI modules are not there.
+     *
+     * @return \Magento\InventorySalesApi\Api\StockResolverInterface|null
+     *
+     * @throws \Magento\Framework\Exception\RuntimeException
+     */
+    private function getStockIndexTableProvider()
+    {
+        if (null === $this->stockIndexTableProvider) {
+            try {
+                $this->stockIndexTableProvider = $this->objectManager->get(
+                    \Magento\InventoryIndexer\Model\StockIndexTableNameResolverInterface::class
+                );
+            } catch (\Exception $exception) {
+                $message = 'Failed to fetch the MSI stock table resolver despite the fact MSI implementation was considered as usable. ';
+                throw new \Magento\Framework\Exception\RuntimeException(__($message . $exception->getMessage()));
+            }
+        }
+
+        return $this->stockIndexTableProvider;
     }
 }
