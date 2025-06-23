5550e274a7d93c4de846b5e26ed02c2bdf9ddbb8
Vadym Honcharuk <vadym.honcharuk@smile-ukraine.com>
2025-06-23 17:50:16 +0300

    [Core] Fix #3623, add supporting getMinPrice() and getMaxPrice() as Magento Core functionality  + removing useless method  + correct parameters for request building

--- src/module-elasticsuite-catalog/Model/ResourceModel/Product/Fulltext/Collection.php
+++ src/module-elasticsuite-catalog/Model/ResourceModel/Product/Fulltext/Collection.php
@@ -13,9 +13,11 @@
  */
 namespace Smile\ElasticsuiteCatalog\Model\ResourceModel\Product\Fulltext;
 
+use Magento\Customer\Model\Group as CustomerGroup;
 use Smile\ElasticsuiteCatalog\Model\Search\Request\Field\Mapper as RequestFieldMapper;
 use Smile\ElasticsuiteCore\Search\Adapter\Elasticsuite\Response\QueryResponse;
 use Smile\ElasticsuiteCore\Search\Request\BucketInterface;
+use Smile\ElasticsuiteCore\Search\Request\MetricInterface;
 use Smile\ElasticsuiteCore\Search\Request\QueryInterface;
 use Smile\ElasticsuiteCore\Search\RequestInterface;
 
@@ -613,6 +615,57 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
         }
 
         return parent::_afterLoad();
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
+     *
+     * Prepares min and max price using Elasticsearch metric aggregation.
+     * Ensures compatibility with Magento Core's getMinPrice()/getMaxPrice().
+     *
+     * @return self
+     */
+    protected function _prepareStatisticsData()
+    {
+        $storeId = $this->getStoreId();
+        $requestName = $this->searchRequestName;
+        $customerGroupId = (int) ($this->_productLimitationFilters['customer_group_id'] ?? CustomerGroup::NOT_LOGGED_IN_ID);
+        $aggregationName = 'collection_price_stats';
+
+        $facets = [
+            'name'       => $aggregationName,
+            'type'       => BucketInterface::TYPE_METRIC,
+            'field'      => 'price.price',
+            'metricType' => MetricInterface::TYPE_STATS,
+            'nestedFilter' => ['price.customer_group_id' => $customerGroupId],
+        ];
+
+        $searchRequest = $this->requestBuilder->create(
+            $storeId,
+            $requestName,
+            0,
+            0,
+            $this->query,
+            [],
+            $this->filters,
+            $this->queryFilters,
+            [$facets],
+        );
+
+        $response     = $this->searchEngine->search($searchRequest);
+        $aggregations = $response->getAggregations();
+
+        $bucket = $aggregations->getBucket($aggregationName);
+        $metrics = current($bucket->getValues())->getMetrics();
+
+        $rate = $this->getCurrencyRate();
+
+        $this->_pricesCount            = (int) ($metrics['count'] ?? 0);
+        $this->_minPrice               = round(((float) ($metrics['min'] ?? 0)) * $rate, 2);
+        $this->_maxPrice               = round(((float) ($metrics['max'] ?? 0)) * $rate, 2);
+        $this->_priceStandardDeviation = round(((float) ($metrics['std_deviation'] ?? 0)) * $rate, 2);
+
+        return $this;
     }
 
     /**
