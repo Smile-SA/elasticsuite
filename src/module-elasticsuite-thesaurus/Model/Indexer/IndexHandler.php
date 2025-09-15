ad6151f45776186f9cbf1210b00c99c69989cc7b
Richard Bayet <richard.bayet@smile.fr>
2025-09-15 17:08:59 +0200

    [Thesaurus] Use stemming in analysis chain to match rules
    This is not pure fuzziness matching, but will allow matching a synonym
    or expansion rule from either the singular or plural form of a common
    noun, for instance.

--- src/module-elasticsuite-thesaurus/Model/Indexer/IndexHandler.php
+++ src/module-elasticsuite-thesaurus/Model/Indexer/IndexHandler.php
@@ -14,10 +14,13 @@
 
 namespace Smile\ElasticsuiteThesaurus\Model\Indexer;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 use Smile\ElasticsuiteCore\Api\Client\ClientInterface;
 use Smile\ElasticsuiteCore\Helper\IndexSettings as IndexSettingsHelper;
 use Smile\ElasticsuiteCore\Helper\Cache as CacheHelper;
 use Smile\ElasticsuiteCore\Api\Index\IndexOperationInterface;
+use Smile\ElasticsuiteCore\Api\Index\IndexSettingsInterface;
 use Smile\ElasticsuiteThesaurus\Model\Index as ThesaurusIndex;
 
 /**
@@ -29,6 +32,31 @@ use Smile\ElasticsuiteThesaurus\Model\Index as ThesaurusIndex;
  */
 class IndexHandler
 {
+    /**
+     * @var string
+     */
+    const THESAURUS_ANALYSIS_USE_STEMMING_XML_PATH = 'smile_elasticsuite_thesaurus_settings/analysis/use_stemming';
+
+    /**
+     * @var string
+     */
+    const STEMMER_FILTER = 'stemmer';
+
+    /**
+     * @var string
+     */
+    const STEMMER_BEFORE_FILTER = 'stemmer_before';
+
+    /**
+     * @var string
+     */
+    const STEMMER_AFTER_FILTER = 'stemmer_after';
+
+    /**
+     * @var string
+     */
+    const STEMMER_OVERRIDE_FILTER = 'stemmer_override';
+
     /**
      * @var ClientInterface
      */
@@ -45,6 +73,16 @@ class IndexHandler
     private $indexManager;
 
     /**
+     * @var IndexSettingsInterface
+     */
+    private $indexSettings;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * @var CacheHelper
      */
     private $cacheHelper;
@@ -55,17 +93,23 @@ class IndexHandler
      * @param ClientInterface         $client              ES client.
      * @param IndexOperationInterface $indexManager        ES index management tool
      * @param IndexSettingsHelper     $indexSettingsHelper Index settings helper.
+     * @param IndexSettingsInterface  $indexSettings       Index settings provider.
+     * @param ScopeConfigInterface    $scopeConfig         Scope config interface.
      * @param CacheHelper             $cacheHelper         ES caching helper.
      */
     public function __construct(
         ClientInterface $client,
         IndexOperationInterface $indexManager,
         IndexSettingsHelper $indexSettingsHelper,
+        IndexSettingsInterface $indexSettings,
+        ScopeConfigInterface $scopeConfig,
         CacheHelper $cacheHelper
     ) {
         $this->client              = $client;
         $this->indexSettingsHelper = $indexSettingsHelper;
         $this->indexManager        = $indexManager;
+        $this->indexSettings       = $indexSettings;
+        $this->scopeConfig         = $scopeConfig;
         $this->cacheHelper         = $cacheHelper;
     }
 
@@ -83,7 +127,7 @@ class IndexHandler
         $indexIdentifier = ThesaurusIndex::INDEX_IDENTIER;
         $indexName       = $this->indexSettingsHelper->createIndexNameFromIdentifier($indexIdentifier, $storeId);
         $indexAlias      = $this->indexSettingsHelper->getIndexAliasFromIdentifier($indexIdentifier, $storeId);
-        $indexSettings   = ['settings' => $this->getIndexSettings($synonyms, $expansions)];
+        $indexSettings   = ['settings' => $this->getIndexSettings($storeId, $synonyms, $expansions)];
         $this->client->createIndex($indexName, $indexSettings);
         $this->indexManager->proceedIndexInstall($indexName, $indexAlias);
         $this->cacheHelper->cleanIndexCache(ThesaurusIndex::INDEX_IDENTIER, $storeId);
@@ -92,12 +136,13 @@ class IndexHandler
     /**
      * Returns index settings.
      *
+     * @param integer  $storeId    Store id.
      * @param string[] $synonyms   Raw synonyms list.
      * @param string[] $expansions Raw expansions list.
      *
      * @return array
      */
-    private function getIndexSettings($synonyms, $expansions)
+    private function getIndexSettings($storeId, $synonyms, $expansions)
     {
         $indexIdentifier = ThesaurusIndex::INDEX_IDENTIER;
 
@@ -128,13 +173,90 @@ class IndexHandler
             'types' => [ "SYNONYM" ],
         ];
 
+        $stemmingFilters = [];
+        if ($this->useStemming($storeId)) {
+            $stemmingFilters = $this->getStemmingTokenFilters($storeId);
+            if (!empty($stemmingFilters)) {
+                $settings['analysis']['filter'] = array_merge($settings['analysis']['filter'], $stemmingFilters);
+            }
+        }
+
+        $settings = $this->addShinglesAnalyzer($settings, array_keys($stemmingFilters));
+
+        $settings = $this->addAnalyzerSettings($settings, 'synonym', $synonyms, array_keys($stemmingFilters));
+        $settings = $this->addAnalyzerSettings($settings, 'expansion', $expansions, array_keys($stemmingFilters));
+
+        return $settings;
+    }
+
+    /**
+     * Returns true if stemming should be used for synonyms and expansions matching.
+     *
+     * @param integer $storeId Store id.
+     *
+     * @return bool
+     */
+    private function useStemming($storeId): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::THESAURUS_ANALYSIS_USE_STEMMING_XML_PATH,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    /**
+     * Retrieves the stemming related token filters defined in the standard analysis configuration for the store.
+     *
+     * @param integer $storeId Store Id.
+     *
+     * @return array
+     */
+    private function getStemmingTokenFilters($storeId): array
+    {
+        $analysisSettings = $this->indexSettings->getAnalysisSettings($storeId);
+
+        $tokenFilters = [];
+
+        $possibleTokenFilters  = [
+            self::STEMMER_BEFORE_FILTER,
+            self::STEMMER_OVERRIDE_FILTER,
+            self::STEMMER_FILTER,
+            self::STEMMER_AFTER_FILTER,
+        ];
+        foreach ($possibleTokenFilters as $tokenFilter) {
+            if (array_key_exists('filter', $analysisSettings) && array_key_exists($tokenFilter, $analysisSettings['filter'])) {
+                $tokenFilters[$tokenFilter] = $analysisSettings['filter'][$tokenFilter];
+            }
+        }
+
+        return $tokenFilters;
+    }
+
+    /**
+     * Append the analyzer dedicated to match and detect multi-words synonyms ("A B,C" or "A-B,C")
+     * to the existing settings.
+     *
+     * @param array $settings   Original settings.
+     * @param array $preFilters Token filters to add in the analysis chain _before_ the shingles detection.
+     *
+     * @return array
+     */
+    private function addShinglesAnalyzer($settings, $preFilters)
+    {
         $settings['analysis']['analyzer']['shingles'] = [
             'tokenizer' => 'whitespace',
-            'filter' => ['lowercase', 'asciifolding', 'analyze_shingle'],
+            'filter' => ['lowercase', 'asciifolding'],
         ];
 
-        $settings = $this->addAnalyzerSettings($settings, 'synonym', $synonyms);
-        $settings = $this->addAnalyzerSettings($settings, 'expansion', $expansions);
+        if (!empty($preFilters)) {
+            $settings['analysis']['analyzer']['shingles']['filter'] = array_merge(
+                $settings['analysis']['analyzer']['shingles']['filter'],
+                $preFilters
+            );
+        }
+
+        $settings['analysis']['analyzer']['shingles']['filter'][] = 'analyze_shingle';
 
         return $settings;
     }
@@ -142,18 +264,26 @@ class IndexHandler
     /**
      * Append an analyzer for a thesaurus to existing settings.
      *
-     * @param array    $settings Original settings.
-     * @param string   $type     Thesaurus type.
-     * @param string[] $values   Thesaurus entries in Lucene format.
+     * @param array    $settings   Original settings.
+     * @param string   $type       Thesaurus type.
+     * @param string[] $values     Thesaurus entries in Lucene format.
+     * @param array    $preFilters Token filters to add in the analysis chain _before_ the synonym/expansion filter.
      *
      * @return array
      */
-    private function addAnalyzerSettings($settings, $type, $values)
+    private function addAnalyzerSettings($settings, $type, $values, $preFilters)
     {
         $settings['analysis']['analyzer'][$type] = [
             'tokenizer' => 'whitespace',
             'filter' => ['lowercase', 'asciifolding'],
         ];
+
+        if (!empty($preFilters)) {
+            $settings['analysis']['analyzer'][$type]['filter'] = array_merge(
+                $settings['analysis']['analyzer'][$type]['filter'],
+                $preFilters
+            );
+        }
 
         if (!empty($values)) {
             $values = $this->prepareSynonymFilterData($values);
@@ -177,10 +307,10 @@ class IndexHandler
      */
     private function prepareSynonymFilterData($rows)
     {
-        $rowMaper = function ($row) {
+        $rowMapper = function ($row) {
             return preg_replace('/([^\s-])[\s-]+(?=[^\s-])/u', '\1_', $row);
         };
 
-        return array_map($rowMaper, $rows);
+        return array_map($rowMapper, $rows);
     }
 }
